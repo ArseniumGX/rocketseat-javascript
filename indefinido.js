8a74dc4cca199503f675ea1335abfdc13b8cca87
Jose Pereira Macedo <arseniumgx@gmail.com>
2021-03-19 16:23:18 -0300

    Aula 5 - indefinido e null

---
+++ indefinido.js
@@ -0,0 +1,7 @@
+let test1 = null // é um objeto definido sem nada dentro, ou seja, vázio
+console.log(typeof test1)
+console.log(test1)
+
+let test2 // está indefinido, não há um valor atribuido a variavel test2
+console.log(typeof test2) // test2 é undefined
+console.log(test2) // retorna undefined, poís não existe valor atribuido à variável
