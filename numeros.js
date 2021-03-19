d09158a1f11e827e296ef178f7368c7387bdcbeb
Jose Pereira Macedo <arseniumgx@gmail.com>
2021-03-19 16:10:51 -0300

    Aula 3 - numeros

---
+++ numeros.js
@@ -0,0 +1,5 @@
+let inteiro = 33
+let inteiroNegativo = -33
+let float = 3.22
+let nan = NaN // não é um numero, tipado como um number
+let infinito = Infinity // retorna true ou false, tbm é tipado como number
