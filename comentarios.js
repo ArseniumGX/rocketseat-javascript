99cb68fee29cc672293b7263171a4698b4ffea82
Jose Pereira Macedo <arseniumgx@gmail.com>
2021-03-19 16:50:33 -0300

    Aula sobre comentarios - update

---
+++ comentarios.js
@@ -0,0 +1,9 @@
+// Comentário em linha
+console.log('Uma forma de fazer')
+
+console.log('Outra forma de ser feito') // compentário de fim de linha
+
+/*
+    Comentário de bloco ou mais de uma linha
+    Usuamente usada para comentar grande volume de blocos de código
+*/
