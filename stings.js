635276df9fde29d2c107149d5b7cdc8f2434b083
Jose Pereira Macedo <arseniumgx@gmail.com>
2021-03-19 16:07:37 -0300

    Aula 2 - strings

---
+++ stings.js
@@ -0,0 +1,9 @@
+let test1 = 'String com aspas simples'
+let test2 = "String com aspas duplas"
+let test3 = `Um template string usando sinais de 'crase'.
+            Com template string é possivel concatenar indormações colocando um 
+            ${test1} no código`
+
+console.log(test1)
+console.log(test2)
+console.log(test3)
